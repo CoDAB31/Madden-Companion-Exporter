23c8a9baed6fb86a6f29c6e8557a6af08ea39491
CoDAB31 <75510292+CoDAB31@users.noreply.github.com>
2020-12-04 19:43:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
 // const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
-
+./nflonxbox2-firebase-adminsdk-3dfs6-fdbfe67a6f.json
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 //   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+// });https://nflonxbox2-default-rtdb.firebaseio.com/
 
 app.set('port', (process.env.PORT || 3001));
 
